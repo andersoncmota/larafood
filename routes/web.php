68899316ca9e02a15c6f4160c7394512f37f6150
Anderson Mota <andersonmota.acm@gmail.com>
2020-06-25 17:03:51 -0300

    Breadcrumb

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ Route::get('admin/create', 'Admin\PlanController@create')->name('plans.create');
 Route::get('admin/plans/{url}', 'Admin\PlanController@show')->name('plans.show');
 Route::get('admin/plans', 'Admin\PlanController@index')->name('plans.index');
 
+Route::get('admin', 'Admin\PlanController@index')->name('admin.index');
+
 Route::get('/', function () {
     return view('welcome');
 });
